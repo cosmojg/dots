86a77799acb01d855522b9e71cf573d41e928d3b
cosmo <cosmo@cosmo.red>
2019-07-14 18:55:55 -0400

    Specify default editor for Julia

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/fish
 
-# Remove the greeting
+# Remove fish greeting
 set fish_greeting
 
 # Set environment variables
@@ -8,5 +8,6 @@ set -gx EDITOR /usr/bin/nvim
 set -gx VISUAL /usr/bin/nvim
 set -gx LANG en_US.UTF-8
 set -gx JULIA_SHELL /bin/bash
+set -gx JULIA_EDITOR /usr/bin/nvim
 set -gx JULIA_NUM_THREADS 8
 set -gx MATLAB_SHELL /bin/bash
