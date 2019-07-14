a9c3f659370fe7b5923952d9b2766a3b74fb5c7d
cosmo <cosmo@cosmo.red>
2019-07-14 18:34:01 -0400

    Add environment variables for Julia and MATLAB

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,3 +7,6 @@ set fish_greeting
 set -gx EDITOR /usr/bin/nvim
 set -gx VISUAL /usr/bin/nvim
 set -gx LANG en_US.UTF-8
+set -gx JULIA_SHELL /bin/bash
+set -gx JULIA_NUM_THREADS 8
+set -gx MATLAB_SHELL /bin/bash
