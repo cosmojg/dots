e941f5c874b9285376f0860c758fbcc64a856d7e
Cosmo <cosmo@cosmo.red>
2023-06-20 21:14:43 -0400

    Update setup.sh

--- .config/dots/setup.sh
+++ .config/dots/setup.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
-# fish configuration
-chsh -s /usr/bin/fish
-fish_update_completions
+# initialize directory structure
+mkdir -p "${HOME}"/code
 
-## pkgfile configuration
-pkgfile -u
-systemctl enable --now pkgfile-update.timer
-
-# git configuration
-git config --global user.name cosmo
+# set global options for git
+git config --global user.name Cosmo
 git config --global user.email cosmo@cosmo.red
 git config --global color.ui true
 git config --global core.editor nvim
+
+# install tmux config with sane defaults
+git clone https://github.com/gpakosz/.tmux.git "${HOME}"/.tmux
+ln -s -f "${HOME}"/.tmux/.tmux.conf "${HOME}"/.tmux.conf
+
+# add catppuccin theme to bat
+git clone https://github.com/catppuccin/bat.git "${HOME}"/.config/bat/themes
+
+# keep pkgfile up-to-date for use with command-not-found
+pkgfile -u
+systemctl enable --now pkgfile-update.timer
+
+# TODO:
+# Add git hooks for shellcheck
+# Migrate more of .bashrc to config.fish (see test.fish)
