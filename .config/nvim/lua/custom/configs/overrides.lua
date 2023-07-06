c8863c184b66116a4e7f599bc1105e605d28ef31
Cosmo <cosmo@cosmo.red>
2023-07-06 14:22:25 -0400

    Use config template for overrides

--- .config/nvim/lua/custom/configs/overrides.lua
+++ .config/nvim/lua/custom/configs/overrides.lua
@@ -1,5 +1,46 @@
 local M = {}
 
+M.treesitter = {
+  ensure_installed = {
+    "bash",
+    "css",
+    "fish",
+    "html",
+    "javascript",
+    "lua",
+    "markdown",
+    "markdown_inline",
+    "python",
+    "tsx",
+    "typescript",
+    "vim",
+  },
+  sync_install = false,
+  auto_install = true,
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+    "prettier",
+  },
+}
+
+-- git support in nvimtree
 M.nvimtree = {
   git = {
     enable = true,
@@ -13,10 +54,6 @@ M.nvimtree = {
       },
     },
   },
-}
-
-M.treesitter = {
-  auto_install = true,
 }
 
 return M
