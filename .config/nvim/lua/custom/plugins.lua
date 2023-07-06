e9638450edd0baf5568124b92fc0cd3ef6815e1b
Cosmo <cosmo@cosmo.red>
2023-07-06 14:03:31 -0400

    Use config template for plugins

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -1,12 +1,52 @@
-local overrides = require "custom.overrides"
+local overrides = require("custom.configs.overrides")
 
-return {
+---@type NvPluginSpec[]
+local plugins = {
 
-  -- override default plugins
-  { "nvim-tree/nvim-tree.lua", opts = overrides.nvimtree },
-  { "nvim-treesitter/nvim-treesitter", opts = overrides.treesitter },
+  -- Override plugin definition options
 
-  -- configure custom plugins
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      -- format & linting
+      {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+          require "custom.configs.null-ls"
+        end,
+      },
+    },
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end, -- Override to setup mason-lspconfig
+  },
+
+  -- override plugin configs
+  {
+    "williamboman/mason.nvim",
+    opts = overrides.mason
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
+
+  -- Install a plugin
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+
   {
     "ricardicus/nvim-magic",
     config = function()
@@ -34,48 +74,19 @@ return {
       }
     end,
   },
+  -- To make a plugin not be loaded
+  -- {
+  --   "NvChad/nvim-colorizer.lua",
+  --   enabled = false
+  -- },
 
-  -- ["gpanders/editorconfig.nvim"] = {
-  --   after = "indent-o-matic",
-  -- },
-  --
-  -- ["Darazaki/indent-o-matic"] = {
-  --   event = "InsertEnter",
-  --   config = function()
-  --     require("indent-o-matic").setup({
-  --       max_lines = 256,
-  --       standard_widths = { 2, 3, 4, 8 },
-  --     })
-  --   end,
-  -- },
-  --
-  -- ["zbirenbaum/copilot.lua"] = {
-  --   after = "nvim-lspconfig",
-  --   config = function()
-  --     vim.defer_fn(function()
-  --       require("copilot").setup({
-  --         copilot_node_command = "nodesixteen",
-  --       })
-  --     end, 100)
-  --   end,
-  -- },
-  --
-  -- ["zbirenbaum/neodim"] = {
-  --   event = "LspAttach",
-  --   config = function ()
-  --     require("neodim").setup({
-  --       alpha = 0.75,
-  --       blend_color = "#000000",
-  --       update_in_insert = {
-  --         enable = true,
-  --         delay = 100,
-  --       },
-  --       hide = {
-  --         virtual_text = true,
-  --         signs = true,
-  --         underline = true,
-  --       },
-  --     })
-  --   end,
-  -- },
+  -- All NvChad plugins are lazy-loaded by default
+  -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
+  -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
+  -- {
+  --   "mg979/vim-visual-multi",
+  --   lazy = false,
+  -- }
 }
+
+return plugins
