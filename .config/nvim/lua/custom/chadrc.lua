cc1dbf1844ed8f4d152c27a51963c5b600a6c9c4
Cosmo <cosmo@cosmo.red>
2023-07-06 13:52:49 -0400

    Use config template and replace missing theme

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -1,12 +1,20 @@
+---@type ChadrcConfig
 local M = {}
+
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
+M.ui = {
+  theme = "catppuccin",
+  theme_toggle = { "catppuccin", "gruvbox_light" },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
+}
 
 M.plugins = "custom.plugins"
 
-M.ui = {
-  theme_toggle = {"catppuccin", "catppuccin_latte"},
-  theme = "catppuccin",
-}
-
+-- check core.mappings for table structure
 M.mappings = require "custom.mappings"
 
 return M
